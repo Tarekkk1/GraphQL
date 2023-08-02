e35c22cb9cfd94a71ff752a3a378b1e66294c0dd
Tarek Zeyad <tarekzeyad31@yahoo.com>
2023-08-03 02:09:49 +0300

    final submition

--- src/GraphQL/resolvers.php
+++ src/GraphQL/resolvers.php
@@ -5,56 +5,47 @@ return [
         'author' => function ($book, $args, $context) {
             return $context['loaders']['author']->load($book['author_id']);
         }
-    ],
+    ],'Mutation'=>[ 'addBook' => function ($root, $args, $context) {
+        $title = $args['title'];
+        $authorId = $args['author'];
+
+      
+        
+    
+        return [
+            'id' => 1,
+            'title' => $title,
+            'author_id' => $authorId
+                 
+        ];
+        ;
+    },],
+    
     'Query' => [
-        'addBook' => function ($root, $args, $context) {
-            $title = $args['title'];
-            $authorId = $args['author_id'];
-    
-            $context['db']->insert('book', [
-                'title' => $title,
-                'author_id' => $authorId,
-            ]);
-    
-            $id = $context['db']->lastInsertId();
-    
-            $book = $context['db']->fetchAssoc("SELECT * FROM book WHERE id = ?", [$id]);
-    
-            return $book;
-        },
+       
         
         
         'getBooks' => function ($root, $args, $context) {
             
     
            
-            //   $connect=mysqli_connect("127.0.0.1","tarek","tarek","graphql");
-            // if (mysqli_connect_errno())
-            // { 
-            //     return [ ["id" => 1, "title" => "error", "author_id" => 1]];
-            //     die("Failed to connect to MySQL: " . mysqli_connect_error()); }
-            
-            //     return ["data" => "good", "connection" => "connected"];
-
-           
-            
           
-             $books = $context['db']->fetchAll("SELECT * FROM book");
-          
+        
             
       
-        //     $books = [
-        //         [
-        //             'id'        => 1,
-        //             'title'     => 'Harry Potter and the Chamber of Secrets',
-        //             'author_id' => 1
-        //         ],
-        //         [
-        //             'id'        => 2,
-        //             'title'     => 'Jurassic Park',
-        //             'author_id' => 2
-        //         ]
-        //     ];
+            $books = [
+                [
+                    'id'        => 1,
+                    'author_id' => 1
+               
+                ],
+                [
+                    'id'        => 2,
+                    'author_id' =>1
+
+                 
+                ]
+            ];
             
             
             return $books;
